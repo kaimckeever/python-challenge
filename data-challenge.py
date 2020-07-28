cc9ec503ef4371b5f5ecb3d6ffdbab3a67a24188
Kai McKeever <kai.mckeever@cfacorp.com>
2020-07-28 08:52:59 -0400

    create lookup dict

--- data-challenge.py
+++ data-challenge.py
@@ -9,7 +9,10 @@ wb = load_workbook(filename="in.xlsx")
 total_sales = wb['Total Sales']
 per_sales = wb['Percentage of Sales']
 lookup = wb['Lookup Table']
-for row in per_sales.iter_rows(min_row=2, max_row=321, values_only=True):
+
+lkp_dict = {k: v for v, k in lookup.iter_rows(min_row=2,values_only=True)}
+
+for row in per_sales.iter_rows(min_row=2,values_only=True):
     yearwk = date(row[1].year,row[1].month,row[1].day).isocalendar()
     col0 = str(yearwk[0]) + '{:02d}'.format(yearwk[1])
     print(col0)
