1e18a432953832986eb1b438aa7c7bc73d938f81
Kai McKeever <kai.mckeever@cfacorp.com>
2020-07-29 20:26:27 -0400

    eliminate needless code consolidate where needed

--- data-challenge.py
+++ data-challenge.py
@@ -1,5 +1,3 @@
-# data-challenge.py
-
 import csv
 from datetime import date
 from openpyxl import load_workbook
@@ -11,7 +9,6 @@ per_sales = wb['Percentage of Sales']
 lookup = wb['Lookup Table']
 
 lkp_dict = {k: v for v, k in lookup.iter_rows(min_row=2,values_only=True)}
-
 total_dict = {(k, l.replace(" ", "").lower()): v for k,l,v in total_sales.iter_rows(min_row=2,values_only=True)}
 
 with open('out.csv', 'w', newline='') as csvfile:
@@ -20,9 +17,7 @@ with open('out.csv', 'w', newline='') as csvfile:
     for prod_id, date_in, size, prod_type, percent in per_sales.iter_rows(min_row=2,values_only=True):
         if percent != 0:
             year, week, _ = date(date_in.year,date_in.month,date_in.day).isocalendar()
-            col0 = str(year) + '{:02d}'.format(week)
-            col1 = lkp_dict[str(prod_id)+str(size)]
-            col2 = size
-            col3 = prod_type
-            col4 = total_dict[(int(col0), col1.replace(" ", "").lower())] * percent
-            writer.writerow([col0,col1,col2,col3,'{:.2f}'.format(col4)])
+            year_week = str(year) + '{:02d}'.format(week)
+            name = lkp_dict[str(prod_id)+str(size)]
+            sales = total_dict[(int(year_week), name.replace(" ", "").lower())] * percent
+            writer.writerow([year_week,name,size,prod_type,'{:.2f}'.format(sales)])
